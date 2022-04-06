a67d683c965de3b4f11ad66f075781425cab795b
Antony Lajes <tonymateuslajes@hotmail.com>
2022-04-06 19:26:34 -0300

    Adição do Drawer Navigation e do Floating Button

--- app/src/main/java/com/example/presencaapp/MainActivity.kt
+++ app/src/main/java/com/example/presencaapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.presencaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.presencaapp.databinding.ActivityMainBinding
+import com.example.presencaapp.ui.fragments.FragmentFloatingButton
 
-class MainActivity : AppCompatActivity() {
+private lateinit var mAct: ActivityMainBinding
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mAct = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mAct.root)
+
+        mAct.toolbarIcon.setOnClickListener(this)
+
+        val supportFragment = supportFragmentManager.beginTransaction()
+        val fragment = FragmentFloatingButton()
+        supportFragment.add(mAct.fragmentFloatingButton.id, fragment)
+    }
+
+    override fun onClick(view: View) {
+        mAct.drawerLayout.open()
     }
 }
